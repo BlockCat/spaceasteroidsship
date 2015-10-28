891c0fb7ff40b819c4725ae47aec4dedcf4d30bb
Robertslob <rjrslob@gmail.com>
2015-10-28 12:27:35 +0100

    cleanup particles

--- framework/src/Particles.hs
+++ framework/src/Particles.hs
@@ -6,7 +6,6 @@ import System.Random
 import Graphics.Gloss.Data.Vector
 import Graphics.Gloss.Data.Point 
 import Graphics.Gloss
-import RandomUtils
 
 import RandomUtils
 
@@ -36,9 +35,9 @@ explosion loc rndGen = (outerExplosion ++ middleExplosion ++ innerExplosion , r3
     where
         speedVar                   = 16
         particleCreator speed pict = randomizedParticle speedVar 180 5 (createParticle loc (speed, 0) 4 pict)
-        (innerExplosion,  r1)      = generateRandom rndGen  ((particleCreator 20 . color yellow . circleSolid) 1) 1000
-        (outerExplosion,  r2)      = generateRandom r1 (particleCreator 30 $ color red    $ circleSolid 1) 1000
-        (middleExplosion, r3)      = generateRandom r1 (particleCreator 25 $ color orange    $ circleSolid 1) 1000
+        (innerExplosion,  r1)      = generateRandom rndGen ((particleCreator 20 . color yellow . circleSolid) 1) 1000
+        (outerExplosion,  r2)      = generateRandom r1     ((particleCreator 30 . color red    . circleSolid) 1) 1000
+        (middleExplosion, r3)      = generateRandom r1     ((particleCreator 25 . color orange . circleSolid) 1) 1000
     
 updateParticles :: Float -> [Particle] -> [Particle]
 updateParticles elapsed xs = filter (\particle -> lifetime particle > 0) (map (updateParticle elapsed) xs) 
